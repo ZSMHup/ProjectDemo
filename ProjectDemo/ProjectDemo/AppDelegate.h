3e64ae7f604a7f7e94b9c486c45383d1f7625aef
张书孟 <zhang_shu_meng@163.com>
2018-06-25 10:32:42 +0800

    [commit]: 新建工程

---
+++ ProjectDemo/ProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectDemo
+//
+//  Created by 张书孟 on 2018/6/25.
+//  Copyright © 2018年 ZSM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectDemo/ProjectDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ProjectDemo
+//
+//  Created by 张书孟 on 2018/6/25.
+//  Copyright © 2018年 ZSM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
